3ab1e17b1fc6f8f4aa4f3bc2812aeed1e8ea0b95
Ubuntu <ubuntu@ip-172-31-92-190.ec2.internal>
2019-09-23 14:35:38 +0000

    basic functionality added

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_messages(username, message):
+    messages.append("{} said: {}<br>".format(username, message))
+
+def get_all_messages():
+    return "<br>".join(messages)
 
 @app.route("/")
 def index():
     return "<h1>Hello There</h1>"
 
+@app.route("/<username>")
+def user(username):
+    return '<h1>Welcome {}!</h1>{}'.format(username, get_all_messages())
+    
+@app.route("/<username>/<message>")
+def send_message(username,message):
+    add_messages(username, message)
+    return redirect ("/" + username)
+
 
 if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+    app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
