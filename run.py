458ac21c2e679e9b91a7eeb57ec9b023004e97c0
Ubuntu <ubuntu@ip-172-31-92-190.ec2.internal>
2019-09-25 11:45:59 +0000

    trying with the model solution

--- run.py
+++ run.py
@@ -2,17 +2,21 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
+
 app = Flask(__name__)
-app.secret_key=os.getenv("SECRET", "randomstring12345")
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
-def add_messages(username, message):
-    now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {'timestamp': now, 'from': username, 'message': message}
-    messages.append(messages_dict)
 
-@app.route("/", methods = ["GET", "POST"])
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    """Main page with instructions"""
     if request.method == "POST":
         session["username"] = request.form["username"]
 
@@ -21,21 +25,17 @@ def index():
 
     return render_template("index.html")
 
-#@app.route("/<username>")
-#def user(username):
-#    return render_template("chat.html", username = username, chat_messages = messages)
-    
+
 @app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    """Display chat messages"""
+    """Add and display chat messages"""
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
-        add_messages(username, message)
+        add_message(username, message)
         return redirect(url_for("user", username=session["username"]))
 
-    return render_template("chat.html", username=username,
-                           chat_messages=messages)
-    
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
+    return render_template("chat.html", username=username, chat_messages=messages)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
